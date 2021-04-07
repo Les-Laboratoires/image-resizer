8ecbc2232d7c0ee26a32a5caaf9a853e8a19f70c
loockeeer <lulu.r1313@gmail.com>
2021-04-07 14:41:11 +0200

    feat: test image ping pong

--- mod.ts
+++ mod.ts
@@ -1,6 +1,53 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
-  event.respondWith(response);
+  const { pathname } = new URL(event.request.url);
+  if(pathname.startsWith("resize")) {
+    if(event.request.method !== "POST") return new Response(null, {
+      status: 405,
+      statusText: "Method Not Allowed",
+    })
+    else return handleResizeRoute(event.request, event)
+  }
+  return new Response(null, {
+    status: 404,
+    statusText: "Page Not Found",
+  })
 });
+
+async function handleResizeRoute(request, event) {
+
+  const contentType = request.headers.get("content-type");
+
+  if(!contentType) {
+    return new Response(
+      JSON.stringify({ error: "please provide 'content-type' header" }),
+      {
+        status: 400,
+        statusText: "Bad Request",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      },
+    )
+  }
+
+
+  if(!contentType.includes('image/')) {
+    return new Response(
+      JSON.stringify({ error: "please send an image" }),
+      {
+        status: 400,
+        statusText: "Bad Request",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      },
+    )
+  }
+
+  return new Response(request.body, {
+    status: 200,
+    headers: {
+      "Content-Type": contentType
+    }
+  })
+}
